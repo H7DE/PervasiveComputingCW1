5436ff760e547259c477637ce8f2d57820077773
Rick <kingkongi@outlook.com>
2015-11-01 19:58:31 +0000

    Updated analytics files

--- grid_analytics.py
+++ grid_analytics.py
@@ -6,16 +6,17 @@ resultsTable = [] #Table of number of nodes and packet transmitted(%)
 #Generate a frequency table of (number of node in simulation, percentage of packets transmitted)
 with sqlite3.connect("sample_db/grid.db") as conn:
     cursor = conn.cursor()
+    exprRootName="grid_topo_node_"
     for i in range(2, 8):
         numNodes = i*i
         amount_pkt = "select count(*) from readings where\
-                readings.experiment_id = 'grid_topo_node_{exprNo}'".format(exprNo=numNodes)
+                readings.experiment_id = '{exprName}{exprNo}'".format(exprName=exprRootName, exprNo=numNodes)
         cursor.execute(amount_pkt)
         noPkts = cursor.fetchall()[0][0];
 
         maxExpect_pkts = "select experiments.expected_no_transmission_per_node \
                 * (experiments.no_nodes - 1) from experiments\
-        where experiments.experiment_id = 'grid_topo_node_{exprNo}'".format(exprNo=numNodes)
+        where experiments.experiment_id = '{exprName}{exprNo}'".format(exprName=exprRootName, exprNo=numNodes)
 
         cursor.execute(maxExpect_pkts)
         maxPkts = cursor.fetchall()[0][0];
