f9391f7effbdd369b107db5b973cfb78848642e9
Rick <kingkongi@outlook.com>
2015-11-01 19:44:17 +0000

    Updated db path

--- random_analytics.py
+++ random_analytics.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 resultsTable = [] #Table of number of nodes and packet transmitted(%)
 
 #Generate a frequency table of (number of node in simulation, percentage of packets transmitted)
-with sqlite3.connect("sample_db/grid.db") as conn:
+with sqlite3.connect("sample_db/random2_40.db") as conn:
     cursor = conn.cursor()
-    for i in range(2, 8):
-        numNodes = i*i
+    exprRootName= "expr_topo_rand_"
+    for i in range(2, 40):
+        numNodes = i
         amount_pkt = "select count(*) from readings where\
-                readings.experiment_id = 'grid_topo_node_{exprNo}'".format(exprNo=numNodes)
+                readings.experiment_id = '{exprName}{exprNo}'".format(exprName=exprRootName, exprNo=numNodes)
         cursor.execute(amount_pkt)
         noPkts = cursor.fetchall()[0][0];
 
         maxExpect_pkts = "select experiments.expected_no_transmission_per_node \
                 * (experiments.no_nodes - 1) from experiments\
-        where experiments.experiment_id = 'grid_topo_node_{exprNo}'".format(exprNo=numNodes)
+        where experiments.experiment_id = '{exprName}{exprNo}'".format(exprName=exprRootName,exprNo=numNodes)
 
         cursor.execute(maxExpect_pkts)
         maxPkts = cursor.fetchall()[0][0];
@@ -25,7 +26,7 @@ with sqlite3.connect("sample_db/grid.db") as conn:
 
 #Create plot
 plt.plot(*zip(*resultsTable))
-plt.title("Comparing WSN network size with %success rate of pkt transmission")
+plt.title("Comparing WSN network size with % pkt loss")
 plt.xlabel('Number of node in simulation')
 plt.ylabel('% of Pkts successfully transmitted')
 plt.show()
